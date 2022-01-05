cfe59fadde5f7bbc9a5783a8cbf23d7e8cf465e4
danielbossier <dbossier21@gmail.com>
2022-01-05 15:29:13 -0800

    added routes to dog controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,40 @@
 class DogsController < ApplicationController
+  def index
+    dogs = Dog.all
+    render json: dogs.as_json
+  end
+
+  def create
+    dog = Dog.new(
+      name: params[:name],
+      weight: params[:weight],
+      sex: params[:sex],
+      breed: params[:breed],
+      age: params[:age],
+    )
+    dog.save
+    render json: dog.as_json
+  end
+
+  def show
+    dog = Dog.find_by(id: params[:id])
+    render json: dog.as_json
+  end
+
+  def update
+    dog = Dog.find_by(id: params[:id])
+    dog.name = params[:name] || dog.name
+    dog.weight = params[:weight] || dog.weight
+    dog.age = params[:age] || dog.age
+    dog.sex = params[:sex] || dog.sex
+    dog.breed = params[:breed] || dog.breed
+    dog.save
+    render json: dog.as_json
+  end
+
+  def destroy
+    dog = Dog.find_by(id: params[:id])
+    dog.destroy
+    render json: { message: "Dog successfully removed." }
+  end
 end
